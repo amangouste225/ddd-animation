37c2f9fcdd62db249b201e30d1c89f82227a3ab9
Amangoua Ange <adagou@yahoo.com>
2024-01-09 15:00:04 +0000

    ui completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,56 @@
+import logo from "./assets/images/logo1.jpg";
+import { BlackBtn } from "./components/BlackBtn";
+
 function App() {
-  return <></>;
+  const getChars = (title) => {
+    let chars = [];
+
+    title.split("").forEach((char) => {
+      chars.push(char);
+    });
+    return chars;
+  };
+
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="nav">
+          <div className="nav_logo">
+            <a href="/">
+              <img src={logo} alt="logo" />
+            </a>
+          </div>
+          <div className="nav_btn">
+            <div className="btn_inner">become a sponsor</div>
+            <span className="circle"></span>
+          </div>
+        </div>
+        <div className="main">
+          <h1>
+            <span>milan</span>
+            <span>renaissance</span>
+            <span>comeback 8th edition</span>
+          </h1>
+        </div>
+        <div className="footer">
+          <div className="footer_privacy">
+            <a href="/">
+              <span>{getChars("privacy policy")}</span>
+            </a>
+          </div>
+          <div className="footer_btn">
+            <BlackBtn>join the waiting list</BlackBtn>
+          </div>
+          <div className="footer_privacy made">
+            <a href="/">
+              <span>{getChars("made by")}</span>
+              <span className="copyright">AMANGOUA</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/BlackBtn.jsx
@@ -0,0 +1,7 @@
+export const BlackBtn = ({ children }) => {
+  return (
+    <div className="btn_wrapper">
+      <button className="btn_black">{children}</button>
+    </div>
+  );
+};
