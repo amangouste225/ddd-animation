49f5fd58a7a3c0d7e0f4d03c9cf0eaa335b03d0f
Amangoua Ange <adagou@yahoo.com>
2024-01-10 16:49:52 +0000

    animtions gsap completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import logo from "./assets/images/logo1.jpg";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
+import { RxCross1 } from "react-icons/rx";
 
 function App() {
   var tl = useRef();
-  const time = gsap.timeline();
-
   // mouse animations
   const xTo = useRef();
   const yTo = useRef();
   const container = useRef();
 
-  // Timeline
-
   // title
   const getChars = (title) => {
     let chars = [];
-
-    title.split("").forEach((char) => {
-      chars.push(char);
+    title.split("").forEach((char, index) => {
+      chars.push(
+        <span key={index} className="chars">
+          {char}
+        </span>
+      );
     });
     return chars;
   };
@@ -33,42 +33,183 @@ function App() {
         (yTo.current = gsap.quickTo(".cursor", "y", {
           duration: 0.5,
           ease: "power3",
-        })),
-        (tl.current = time
-          .to(
-            ".lgo",
-            {
-              y: "100%",
-              ease: "back.in",
-            },
-            "<"
-          )
-          .to(".inner_text span", {
-            top: "0%",
-            ease: "back.inOut",
-          }));
+        }));
     },
     { scope: container }
   );
-
-  useEffect(() => {}, [container]);
 
   const moveShape = contextSafe((e) => {
     xTo.current(e.clientX);
     yTo.current(e.clientY);
   });
 
-  const onClicked = contextSafe(() => {
-    tl.current.reversed();
-  });
+  useGSAP(
+    () => {
+      tl.current = gsap
+        .timeline()
+        .to(
+          ".btn_wrapper",
+          {
+            ease: "back.in",
+            y: "300px",
+          },
+          "start"
+        )
+        .to(
+          ".heading_inner",
+          {
+            ease: "back.in",
+            y: "100px",
+          },
+          "start"
+        )
+        .to(
+          ".content",
+          {
+            css: { opacity: 1 },
+          },
+          "open"
+        )
+
+        .from(
+          ".form",
+          {
+            y: "200px",
+            ease: "back",
+            stagger: 0.05,
+          },
+          "<"
+        )
+        .to(
+          ".form",
+          {
+            width: "500px",
+            ease: "back(3)",
+          },
+          ">"
+        )
+        .to(
+          ".content2",
+          {
+            css: { filter: "blur(10px)", opacity: 1 },
+          },
+          "<"
+        )
+        .to(
+          ".input",
+          {
+            css: { display: "block" },
+            ease: "back.out",
+          },
+          "<"
+        )
+        .from(
+          ".content_title",
+          {
+            y: "540px",
+            stagger: 0.05,
+            ease: "back",
+            rotation: 20,
+          },
+          "<"
+        )
+        .from(
+          ".content_para",
+          {
+            stagger: 0.05,
+            ease: "back",
+            y: "550px",
+            rotation: -10,
+          },
+          "<"
+        )
+        .from(
+          ".btn_spam",
+          {
+            stagger: 0.05,
+            ease: "back",
+            y: "550px",
+            rotation: -10,
+          },
+          "<"
+        )
+        .from(".btn_close", {
+          scale: 0,
+          ease: "back",
+        })
+        .to(
+          ".lgo",
+          {
+            y: "300px",
+            stagger: 0.05,
+            ease: "back.in",
+          },
+          "<"
+        )
+        .to(".inner_text span", {
+          top: "0%",
+          ease: "back",
+        })
+        .to(
+          ".blue1",
+          {
+            css: {
+              zIndex: "9999",
+              backgroundColor: "#382968",
+              opacity: 0.9,
+            },
+            duration: 1,
+          },
+          "-=2"
+        )
+        .reverse();
+    },
+    { scope: container }
+  );
+
+  const onClicked = () => {
+    tl.current.reversed(!tl.current.reversed());
+  };
 
   return (
-    <div
+    <section
       className="container"
       ref={container}
       onMouseMove={(e) => moveShape(e)}
     >
-      <div></div>
+      <div className="blue1">
+        <div className="content">
+          <div className="btn_close" onClick={onClicked}>
+            <RxCross1 size="25" />
+          </div>
+          <div className="content-wrapper">
+            <h1 className="content_title"> {getChars("join the")}</h1>
+            <h1 className="content_title"> {getChars("waitlist")}</h1>
+          </div>
+
+          <p className="content_para">
+            Subscribe now to get 50% off tickets deal and latest news on the 8th
+            edition
+          </p>
+          <div className="btn_spam">
+            <button>no spam</button>
+            <button>unscribe anytime</button>
+          </div>
+          <form className="form">
+            <input
+              type="text"
+              placeholder="insert your email"
+              className="input"
+            />
+            <button>subscribe</button>
+          </form>
+        </div>
+      </div>
+      <div className="content2">
+        <h1>
+          Website <br /> content
+        </h1>
+      </div>
       <div className="cursor"></div>
       <div className="wrapper">
         <div className="nav">
@@ -91,33 +232,27 @@ function App() {
           </div>
         </div>
         <div className="main">
-          <h1>
-            <span>
-              <div>{getChars("milan")}</div>
-            </span>
-            <span> {getChars("renaissance")} </span>
-            <span> {getChars("comeback 8th edition")} </span>
+          <h1 className="heading">
+            <div className="heading_inner">
+              <span>milan</span>
+            </div>
+            <div className="heading_inner">
+              <span>renaissance</span>
+            </div>
+            <div className="heading_inner">
+              <span>{getChars("comeback 8th edition")}</span>
+            </div>
           </h1>
-
           <div className="btn_wrapper" onClick={onClicked}>
             <div className="btn_black">
-              <span>{getChars("join the waiting list")}</span>
-            </div>
-          </div>
-
-          <div className="digital">
-            <div>Digital </div>
-            <div>design</div>
-            <div>days</div>
-            <div>
-              <span>fall 2024</span>
+              <span>join the waiting list</span>
             </div>
           </div>
         </div>
         <div className="footer">
           <div className="footer_privacy">
             <a href="/">
-              <span>{getChars("privacy policy")}</span>
+              <div>{getChars("privacy policy")}</div>
             </a>
           </div>
           <div className="footer_btn"></div>
@@ -133,7 +268,7 @@ function App() {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
